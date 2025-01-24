67fee59b69d771f1ed78c47f64c7328b095352f2
Sam Walford <samwalford52@yahoo.com>
2025-01-24 06:42:32 -0300

    Add all project files for Mod_31A

---
+++ Mod_31.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Mod_31 Project")
+st.write("This is a placeholder Streamlit app.")
+
---
+++ setup.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/.streamlit/ && echo "[server]
+headless = true
+enableCORS = false
+port = $PORT
+" > ~/.streamlit/config.toml
